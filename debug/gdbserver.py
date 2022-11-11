ed38e98ac3e87fe643b7b7a77b0954e0d927b734
Tim Newsome <tim@sifive.com>
2022-11-10 16:10:49 -0800

    Add CeaseStepiTest.
    Test that we work correctly when the hart we're debugging ceases to
    respond during stepi.

--- debug/gdbserver.py
+++ debug/gdbserver.py
@@ -1799,6 +1799,29 @@ class EbreakTest(GdbSingleHartTest):
         output = self.gdb.c()
         assertIn("_exit", output)
 
+class CeaseStepiTest(ProgramTest):
+    """Test that we work correctly when the hart we're debugging ceases to
+    respond."""
+    def early_applicable(self):
+        return self.hart.support_cease
+
+    def test(self):
+        self.gdb.b("main")
+        output = self.gdb.c()
+        assertIn("Breakpoint", output)
+        assertIn("main", output)
+
+        self.gdb.p("$pc=cease")
+        self.gdb.stepi(wait=False)
+        self.gdb.expect(r"\S+ became unavailable.")
+        self.gdb.interrupt()
+        try:
+            self.gdb.p("$pc")
+            assert False, ("Registers shouldn't be accessible when the hart is "
+                           "unavailable.")
+        except CouldNotReadRegisters:
+            pass
+
 class CeaseMultiTest(GdbTest):
     """Test that we work correctly when a hart ceases to respond (e.g. because
     it's powered down)."""
